15dda29ec17261cbd1aa8e9e8094be45ee89f9ad
cglotr <amir.ariffin.1992@gmail.com>
2022-12-05 21:40:20 +0800

    nicer error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -10,13 +11,21 @@ fn main() {
         process::exit(1);
     });
 
-    println!("searching for {}", config.query);
+    println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1)
+    }
+}
 
-    println!("With text:\n{contents}")
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
